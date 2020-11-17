3f45d10dd33e05308bac04c55fb2cb55136ec7f4
Jon amaya <amaya.jon95@gmail.com>
2020-11-17 17:30:52 -0600

    thinger io added

--- BuggE Arduino Source Code/src/main.cpp
+++ BuggE Arduino Source Code/src/main.cpp
@@ -6,11 +6,23 @@
 #include <NewPing.h>
 #include <Adafruit_LSM303DLH_Mag.h>
 #include <Adafruit_Sensor.h>
-
-
+#include <LSM303.h>
+#include <TinyGsmClient.h>
+#include <ThingerTinyGSM.h>
 //ultrasonic modules and distance to trigger stop
 #define SONAR_NUM 3      
 #define MAX_DISTANCE 12 
+#define TINY_GSM_MODEM_SIM800
+
+
+//SIM800L SETUP
+
+
+
+
+
+
+
 
 //ultrasonic sensor sweeping 
 boolean sonarPing = false;
@@ -57,14 +69,32 @@ int increment = 0; //???
 
 
 //compass axis
-Adafruit_LSM303DLH_Mag_Unified mag = Adafruit_LSM303DLH_Mag_Unified(12345).;
+//Adafruit_LSM303DLH_Mag_Unified mag = Adafruit_LSM303DLH_Mag_Unified(12345).;
+
+#define USERNAME "ttuRover"
+#define DEVICE_ID "Arduino"
+#define DEVICE_CREDENTIAL "raider"
+
+// use your own APN config
+#define APN_NAME "epc.tmobile.com"
+#define APN_USER ""
+#define APN_PSWD ""
+
+// set your cad pin (optional)
+#define CARD_PIN ""
+
+ThingerTinyGSM thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL, Serial2);
 
 
+
+
+
+LSM303 compass;
 int *compassX = 0;
 int *compassY = 0 ;
 int *compassZ = 0;
 const int compassOffset = 10;
-int *compassHeading;
+float *compassHeading;
 int *compassHeadingOne;
 int *compassHeadingTwo;
 int *roverCycle;
@@ -98,7 +128,15 @@ void updateGPS(){
 }
 
 void updateCompass(){
-  
+compass.read();
+*compassHeading = compass.heading();
+*compassHeading+=5.47;
+
+Serial.print("Compass Reading: ");
+Serial.println(*compassHeading
+);
+
+ /* 
   sensors_event_t event;
   mag.getEvent(&event);
   
@@ -113,6 +151,9 @@ void updateCompass(){
   Serial.print("Compass Heading: ");
   Serial.println(heading);
   Serial.println(*compassHeading);
+
+
+  */
 }
 
 void roverSetup()
@@ -124,13 +165,17 @@ void roverSetup()
     updateGPS();                                         // Update gps data
     satelliteCount = (int)(gps.satellites.value());   // Query Tiny GPS for the number of Satellites Acquired       
   }
-    /* Initialise the sensor */
-  if (!mag.begin()) {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while (1)
-      ;
-  }
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  
+  /*
+  Calibration values; the default values of +/-32767 for each axis
+  lead to an assumed magnetometer bias of 0. Use the Calibrate example
+  program to determine appropriate values for your particular unit.
+  */
+  compass.m_min = (LSM303::vector<int16_t>){-380, -557, -442};
+  compass.m_max = (LSM303::vector<int16_t>){+648, +518, +498};
     
    updateGPS();
    updateCompass();
@@ -196,6 +241,23 @@ void setup()
   motorBackRight.begin(motorBackRightPWM, motorBackRightEnableA, mototBackRightEnableB);
   motorBackLeft.begin(motorBackLeftPWM, motorBackLeftEnableA, motorBackLeftEnableB);
   roverSetup();
+
+  
+  // Serial for AT commands (can be higher with HW Serial, or even lower in SW Serial)
+  Serial2.begin(57600);
+
+  // set APN (you can remove user and password from call if your apn does not require them)
+  thing.setAPN(APN_NAME, APN_USER, APN_PSWD);
+
+  // set PIN (optional)
+  // thing.setPIN(CARD_PIN);
+
+  // resource input example (i.e, controlling a digitalPin);
+  pinMode(LED_BUILTIN, OUTPUT);
+  thing["led"] << digitalPin(LED_BUILTIN);
+
+  // resource output example (i.e. reading a sensor value)
+  thing["millis"] >> outputValue(millis());
 }
 
 void loop()
